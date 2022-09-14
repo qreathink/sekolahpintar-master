53f607b336600e7710bbd0f9080c63c190b4cc8a
Ricky Sutanto <ricky@sali-evo.com>
2022-09-14 17:01:21 +0700

    ricky update 1

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ Route::get('/home', function () {
     return redirect()->route('admin.home');
 });
 
-Auth::routes(['register' => false]);
+Auth::routes(['register' => false]); // ini gak perlu
+
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
     Route::get('/', 'HomeController@index')->name('home');
